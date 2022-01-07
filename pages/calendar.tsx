8eb9935a26828c991dbaba5d6b43063c10c3b98f
F1sxher <fisherjc2008@gmail.com>
2022-01-07 12:08:36 -0500

    updated calendar

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -3,7 +3,7 @@ const Calendar: NextPage = () => {
   return (
     <div className="flex justify-center items-center w-screen vh-default-main bg-gray-900">
       <main className="bg-gray-900">
-        
+      <iframe src="https://calendar.google.com/calendar/embed?wkst=1&bgcolor=%23F09300&ctz=America%2FNew_York&title&src=a2lscm95cm9ib3RpY3NAZ21haWwuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23EF6C00&color=%23D81B60" className="border-0 vh-default-main w-screen" scrolling="no"></iframe>
       </main>
     </div>
   );
