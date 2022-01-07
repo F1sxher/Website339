138840dd89a9957492fb292edba0961e64699e8d
F1sxher <fisherjc2008@gmail.com>
2022-01-07 12:23:43 -0500

    fix(links): ALL links should be fixed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,68 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/links',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/links/facebook',
+        destination: 'https://facebook.com/kilroyrobotics',
+        permanent: true,
+      },
+      {
+        source: '/links/fb',
+        destination: 'https://facebook.com/kilroyrobotics',
+        permanent: true,
+      },
+      {
+        source: '/links/github',
+        destination: 'https://github.com/FIRST-TEAM-339',
+        permanent: true,
+      },
+      {
+        source: '/links/gh',
+        destination: 'https://github.com/FIRST-TEAM-339',
+        permanent: true,
+      },
+      {
+        source: '/links/thebluealliance',
+        destination: 'https://thebluealliance.com/team/339',
+        permanent: true,
+      },
+      {
+        source: '/links/tba',
+        destination: 'https://thebluealliance.com/team/339',
+        permanent: true,
+      },
+      {
+        source: '/links/twitter',
+        destination: 'https://twitter.com/kilroyrobotics',
+        permanent: true,
+      },
+      {
+        source: '/links/firstinspires',
+        destination: 'https://firstinspires.org',
+        permanent: true,
+      },
+      {
+        source: '/links/usfirst',
+        destination: 'https://firstinspires.org',
+        permanent: true,
+      },
+      {
+        source: '/links/first',
+        destination: 'https://firstinspires.org',
+        permanent: true,
+      },
+      {
+        source: '/links/frc',
+        destination: 'https://firstinspires.org',
+        permanent: true,
+      },
+    ]
+  },
 };
