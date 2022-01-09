cd430b5848d9fc3514caff47636bd6250e801346
F1sxher <fisherjc2008@gmail.com>
2022-01-09 16:05:17 -0500

    contact page starts

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,13 +1,104 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from "nodemailer";
 
 type Data = {
-  name: string
+  status: "success" | "error";
+  message: string;
+  replyEmail?: string;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.body)
+  if (!req.body.content) return res.status(400).json({ status: "error", message: "invalid, empty, or no content" })
+  if (!req.body.replyEmail) return res.status(400).json({ status: "error", message: "invalid, empty, or no reply email" })
+
+  const transporter = nodemailer.createTransport({
+    host: process.env["mail.host"],
+    port: process.env["mail.port"] || 587,
+    secure: process.env["mail.port"] == "465",
+    auth: {
+      user: process.env["mail.user"],
+      pass: process.env["mail.pass"],
+    }
+  });
+
+  try { 
+    await transporter.sendMail({
+      from: `"Kilroy Web Mailer" <${process.env["mail.user"]}>`,
+      to: "kilroyrobotics@gmail.com",
+      replyTo: req.body.replyEmail,
+      subject: `Kilroy Contact - ${req.body. req.body.replyEmail}`,
+      html: `<table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF;" width="100%">
+      <tbody>
+      <tr>
+      <td>
+      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+      <tbody>
+      <tr>
+      <td>
+      <table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+      <tbody>
+      <tr>
+      <td class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
+      <table border="0" cellpadding="0" cellspacing="0" class="heading_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+      <tbody><tr>
+      <td style="width:100%;text-align:center;">
+      <h1 style="margin: 0; color: #555555; font-size: 40px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; line-height: 120%; text-align: center; direction: ltr; font-weight: normal; letter-spacing: normal; margin-top: 0; margin-bottom: 0;"><strong>Kilroy Website Message</strong></h1>
+      </td>
+      </tr>
+      </tbody></table>
+      <table border="0" cellpadding="0" cellspacing="0" class="heading_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+      <tbody><tr>
+      <td style="width:100%;text-align:center;">
+      <h2 style="margin: 0; color: #555555; font-size: 18px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; line-height: 120%; text-align: left; direction: ltr; font-weight: normal; letter-spacing: normal; margin-top: 0; margin-bottom: 0;">Reply Address:&nbsp;<strong>${req.body.replyEmail}</strong></h2>
+      </td>
+      </tr>
+      </tbody></table>
+      <table border="0" cellpadding="10" cellspacing="0" class="divider_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+      <tbody><tr>
+      <td>
+      <div align="center">
+      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+      <tbody><tr>
+      <td class="divider_inner" style="font-size: 1px; line-height: 1px; border-top: 1px solid #BBBBBB;"><span>&hairsp;</span></td>
+      </tr>
+      </tbody></table>
+      </div>
+      </td>
+      </tr>
+      </tbody></table>
+      <table border="0" cellpadding="10" cellspacing="0" class="text_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+      <tbody><tr>
+      <td>
+      <div style="font-family: sans-serif">
+      <div style="font-size: 14px; mso-line-height-alt: 16.8px; color: #555555; line-height: 1.2; font-family: Arial, Helvetica Neue, Helvetica, sans-serif;">
+      <p style="margin: 0; font-size: 14px;">${req.body.content}</p>
+      </div>
+      </div>
+      </td>
+      </tr>
+      </tbody></table>
+      </td>
+      </tr>
+      </tbody>
+      </table>
+      </td>
+      </tr>
+      </tbody>
+      </table>
+      
+      </td>
+      </tr>
+      </tbody>
+      </table>`,  
+    })
+
+    res.status(200).redirect(307, "/success?type=contact").json({ status: "success", message: "Email Sent" })
+  } catch {
+    res.status(500).redirect(307, "/500").json({ status: "error", message: "unknown server error" })
+  }
 }
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,34 +1,152 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 
-const Err404: NextPage = () => {
+const Contact: NextPage = () => {
   return (
-    <div className="flex justify-center items-center w-screen vh-default-main bg-gray-900">
-      <main className="bg-gray-900">
-        <span className="flex justify-center items-center w-screen x1:inline text-white">
-          <h1 className="text-red-400 text-8xl x1:inline font-extrabold">
-            404
-          </h1>
-          <p className="text-8xl">{" | "}</p>
-          <h2 className="text-left">
-            <span className="font-bold text-5xl">Page Not Found</span>
-            <br />
-            <span className="text-3xl text-gray-400">
-              The Page you were looking for was not found.{" "}
-            </span>
-          </h2>
-        </span>
-        <div className="mt-5 mx-auto sm:flex sm:justify-center md:mt-8 p-5 m-10">
-          <div className="rounded-md shadow flex items-center justify-center py-3 border border-transparent text-base font-medium text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10 m-5">
-            <Link href="/">Go Home</Link>
-          </div>
-          <div className="rounded-md shadow flex items-center justify-center py-3 border border-transparent text-base font-medium text-blue-600 bg-white hover:bg-gray-300 md:py-4 md:text-lg md:px-10 m-5">
-            <Link href="/support">Get Support</Link>
+    <div className="flex justify-center items-center w-screen min-h-[95vh] bg-gray-900">
+      <main className="bg-gray-900 min-h-full">
+        <br />
+        <div className="w-full max-w-lg text-center">
+          <div className="px-4 sm:px-0">
+            <h3 className="text-4xl font-medium leading-6 text-gray-300">Contact</h3>
+            <p className="mt-1 text-sm text-gray-400">
+              This information will be sent to a select few members and adults, and will not be shared elsewhere. We respond within a few days, assuming there is not a break or holiday during the time when this is sent. Your response will be sent directly into the inbox of the reply email address listed.
+            </p>
           </div>
         </div>
+        <br />
+        <form action="/api/contact" method="POST">
+          <div className="shadow sm:rounded-md sm:overflow-hidden h-full">
+            <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+              <div className="grid grid-cols-3 gap-6">
+                <div className="col-span-3 sm:col-span-2">
+                  <label htmlFor="subject" className="block text-sm font-medium text-gray-700">
+                    Subject
+                  </label>
+                  <div className="mt-1 flex rounded-md shadow-sm">
+                    <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                      Kilroy Contact
+                    </span>
+                    <input
+                      type="text"
+                      name="subject"
+                      id="subject"
+                      className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                      placeholder="Interested in joining kilroy"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="grid grid-cols-3 gap-6">
+                <div className="col-span-3 sm:col-span-2">
+                  <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                    Name
+                  </label>
+                  <div className="mt-1 flex rounded-md shadow-sm">
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
+                      placeholder="John Smith"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="grid grid-cols-3 gap-6">
+                <div className="col-span-3 sm:col-span-2">
+                  <label htmlFor="replyEmail" className="block text-sm font-medium text-gray-700">
+                    Reply Address <span className="text-red-500 font-bold">*</span>
+                  </label>
+                  <div className="mt-1 flex rounded-md shadow-sm">
+                    <input
+                      type="email"
+                      name="replyEmail"
+                      id="replyEmail"
+                      className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
+                      placeholder="you@example.com"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="content" className="block text-sm font-medium text-gray-700">
+                  Message <span className="text-red-500 font-bold">*</span>
+                </label>
+                <div className="mt-1">
+                  <textarea
+                    id="content"
+                    name="content"
+                    rows={3}
+                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                    placeholder="Type your message here"
+                  />
+                </div>
+              </div>
+
+              <fieldset>
+                <div>
+                  <legend className="text-base font-medium text-gray-900">Priority <span className="text-red-500 font-bold">*</span></legend>
+                  <p className="text-sm text-gray-500">How quickly should we get back to you?</p>
+                </div>
+                <div className="mt-4 space-y-4">
+                  <div className="flex items-center">
+                    <input
+                      id="priority-urgent"
+                      name="priority"
+                      type="radio"
+                      className="focus:ring-red-500 h-4 w-4 text-red-600 border-gray-300"
+                    />
+                    <label htmlFor="priority-urgent" className="ml-3 block text-sm font-medium text-gray-700">
+                      Urgent - We will get back to you as soon as possible
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input
+                      id="priority-medium"
+                      name="priority"
+                      type="radio"
+                      className="focus:ring-amber-500 h-4 w-4 text-amber-600 border-gray-300"
+                    />
+                    <label htmlFor="priority-medium" className="ml-3 block text-sm font-medium text-gray-700">
+                      Medium - We will get back to you soon, most likely within a few buisness days
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input
+                      id="priority-low"
+                      name="priority"
+                      type="radio"
+                      className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300"
+                    />
+                    <label htmlFor="priority-low" className="ml-3 block text-sm font-medium text-gray-700">
+                      Low Priority - We will get back to you when we are available
+                    </label>
+                  </div>
+                </div>
+              </fieldset>
+
+              <p className="px-5 text-sm text-red-500">
+                Required *
+              </p>
+            </div>
+
+            <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button
+                type="submit"
+                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Send Message
+              </button>
+            </div>
+          </div>
+        </form>
       </main>
     </div>
   );
 };
 
-export default Err404;
+export default Contact;
